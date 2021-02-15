be03ec501b8651a626d0a69f3f1691a2a2a3c188
Seth Zyg <zygster11@gmail.com>
2021-02-15 17:24:14 -0600

    Added a Mixes table for each Artist. They can add to a carousel mixes they like from the internet. Carousel not working however.

---
+++ models/mixes.js
@@ -0,0 +1,24 @@
+module.exports = function (sequelize, DataTypes) {
+  var Mixes = sequelize.define("Mixes", {
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+
+  Mixes.associate = function (models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Mixes.belongsTo(models.Artists, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
+  return Mixes;
+};
