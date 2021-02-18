3198bfe8ca999f3aaee972c6cfbccd76840c05f1
Seth Zyg <zygster11@gmail.com>
2021-02-18 15:08:08 -0600

    Fixed 'search by city' on all-artists page.

--- routes/routes.js
+++ routes/routes.js
@@ -55,13 +55,7 @@ module.exports = function (app) {
       order: [[{ model: db.Blogs }, "createdAt", "DESC"]],
       include: [db.Blogs, db.Extras, db.Mixes],
     }).then((data) => {
-      if (data.dataValues.Mixes) {
-        for (let i = 0; i < data.dataValues.Mixes.length; i++) {
-          if (data.dataValues.Mixes[i].id === 1) {
-            data.dataValues.Mixes[i].isActive = true;
-          }
-        }
-      }
+
 
       res.render("profile", {
         artist: data.dataValues,
@@ -160,7 +154,7 @@ module.exports = function (app) {
         res.status(401).json(err);
       });
   });
-  
+
     // Route for logging user out
     app.get("/logout", (req, res) => {
       req.logout();
