f1ee2e67832513244f3257f7c4de50078ec92ebd
DMC <dorykahale@gmail.com>
2021-02-19 23:52:24 -0600

    updated drum pads

--- public/assets/js/drums.js
+++ public/assets/js/drums.js
@@ -1,9 +1,19 @@
 $(document).ready(() => {
-  // <<------ Drum Emulator ------>>
 
-q
+var padOne = new Audio("/assets/drum-main-sounds/kit1/kick-1.wav");
+var padTwo = new Audio("/assets/drum-main-sounds/kit1/snare-1.wav");
+var padThree = new Audio("/assets/drum-main-sounds/kit1/closed-hat-1.wav");
+var padFour = new Audio("/assets/drum-main-sounds/kit1/clap-1.wav");
+var padFive = new Audio("/assets/drum-main-sounds/kit2/kick-2.wav");
+var padSix = new Audio("/assets/drum-main-sounds/kit2/snare-2.wav");
+var padSeven = new Audio("/assets/drum-main-sounds/kit2/closed-hat-2.wav");
+var padEight = new Audio("/assets/drum-main-sounds/kit2/clap-2.wav");
+var padNine = new Audio("/assets/drum-main-sounds/kit3/kick-3.wav");
+var padTen = new Audio("/assets/drum-main-sounds/kit3/snare-3.wav");
+var padEleven = new Audio("/assets/drum-main-sounds/kit3/closed-hat-3.wav");
+var padTwelve = new Audio("/assets/drum-main-sounds/kit3/clap-3.wav");
 
-  $("body").on("keydown", function (e) {
+  $().on("keydown", function (e) {
     if ($("#checkbox").is(":checked") == false) {
       e.stopImmediatePropagation();
     } else {
